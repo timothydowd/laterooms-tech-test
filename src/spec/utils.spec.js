791f02469342bca60c5a2def7e0801f44a00398c
timothydowd <timdowd1984@gmail.com>
2019-07-09 11:13:46 +0100

    implemented deepFreeze into utility testing to ensure no mutations in state occur

--- src/spec/utils.spec.js
+++ src/spec/utils.spec.js
@@ -1,10 +1,11 @@
 const { expect } = require('chai')
 const { getUniqueFacils } = require('../utils/utils')
+const  deepFreeze  = require('deep-freeze')
 // const data = require('../data/data.json')
 
 describe('getUniqueFacils', () => {
     it('when passed an empty array it returns an empty array', () => {
-        expect(getUniqueFacils([])).to.eql([])
+        expect(getUniqueFacils(deepFreeze([]))).to.eql([])
       });
       it('when passed 1 hotel without facilities, returns an empty array', () => {
         const hotels = [{
@@ -12,6 +13,8 @@ describe('getUniqueFacils', () => {
           "starRating": 5,
           "facilities": []
         }]
+
+        deepFreeze(hotels)
         expect(getUniqueFacils(hotels)).to.eql([])
       });
       it('when passed 1 hotel with facilities, returns those facilities in an array', () => {
@@ -20,6 +23,8 @@ describe('getUniqueFacils', () => {
           "starRating": 5,
           "facilities": ["car park", "pool"]
         }]
+
+        deepFreeze(hotels)
         expect(getUniqueFacils(hotels)).to.eql(["car park", "pool"])
       });
       it('when passed 2 hotels with same facilities, returns those facilities without any duplicates', () => {
@@ -33,6 +38,8 @@ describe('getUniqueFacils', () => {
           "starRating": 5,
           "facilities": ["car park", "pool"]
         }]
+
+        deepFreeze(hotels)
         expect(getUniqueFacils(hotels)).to.eql(["car park", "pool"])
       });
       it('when passed 2 hotels with differing facilities, returns all facilities', () => {
@@ -46,6 +53,8 @@ describe('getUniqueFacils', () => {
           "starRating": 5,
           "facilities": ["bar", "lounge"]
         }]
+
+        deepFreeze(hotels)
         expect(getUniqueFacils(hotels)).to.eql(["car park", "pool", "bar", "lounge"])
       });
       it('when passed a number of hotels with differing overlapping facilities, returns all unique facilities', () => {
@@ -70,6 +79,8 @@ describe('getUniqueFacils', () => {
           "facilities": ["bar", "sauna", "restaurant"]
         },
       ]
+
+        deepFreeze(hotels)
         expect(getUniqueFacils(hotels)).to.eql(["car park", "pool", "gym", "bar", "lounge", "terrace", "sauna", "restaurant"])
       });
 });
